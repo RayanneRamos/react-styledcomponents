9dc8fcf681e9e8f6aef0cde40f64f65ab02d7755
Rayanne Ramos <rayanneramosoficial@gmail.com>
2022-04-06 16:55:33 -0300

    Criação do componente Item

---
+++ src/components/Item/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Item } from './style';
+
+export default ({ type, from, value }) => {
+  return (
+    <Item>
+      <span className="text">{type}</span>
+      <span>{from}</span>
+      <span>{value}</span>
+    </Item>
+  );
+};
---
+++ src/components/Item/style.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Item = styled.div`
+  display: flex;
+  flex-direction: column;
+  .text {
+    font-weight: bold;
+  }
+`;
